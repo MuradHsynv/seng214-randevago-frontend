dd151b783daf74683ba7332c437f0d5e8e91e556
Cem Serimer <cdserimer@icloud.com>
2025-05-15 15:47:34 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'randevago',
+  webDir: 'dist'
+};
+
+export default config;
